bc3190c79183c69241d4fbb26d0a89daf5c86ff9
Steven Raden <stevengraden@gmail.com>
2015-05-03 16:06:19 -0700

    new data set and sliders working

--- tennisScatter.js
+++ tennisScatter.js
@@ -1,11 +1,18 @@
 /// <reference path="typings/d3/d3.d.ts"/>
+/**
+ * X = Rank (expand x axis on change)
+ * Y = Total wins (grouped by Surface)
+ * Slider 1 = # wins
+ * Slider 2 = Rank
+ * Dropdown = Gender
+ * Dropdown = Surface
+ */
+
 "use strict";
 
-$(document).ready(function() {
-
 var margin = {top: 20, right: 20, bottom: 30, left: 50};
-	var w = 640 - margin.left - margin.right;
-	var h = 480 - margin.top - margin.bottom;
+var w = 640 - margin.left - margin.right;
+var h = 480 - margin.top - margin.bottom;
 
 var col = d3.scale.category10();
 
@@ -19,85 +26,99 @@ var svg = d3.select("#graph").append("svg")
 .append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scale.linear()
-			.domain([0, 1000])
-			.range([0, w]);
-
-var xAxis = d3.svg.axis()
-			.scale(x)
-			.orient("bottom"); 
-
-var y = d3.scale.linear()
-			.domain([0, 14500])
-			.range([h, 0]);
-
-var yAxis = d3.svg.axis()
-			.scale(y)
-			.orient("left");
-
-svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(0," + h + ")")
-	.call(xAxis)
-.append("text")
-	.attr("x", w)
-	.attr("y", -6)
-	.style("text-anchor", "end")
-	.text("Price");
-
-svg.append("g")
-	.attr("class", "axis")
-	.call(yAxis)
-.append("text")
-	.attr("transform", "rotate(-90)")
-	.attr("y", 6)
-	.attr("dy", ".71em")
-	.style("text-anchor", "end")
-	.text("True Value");    
+var maxWins = 0; //Global store for wins
+var maxRank = 0; //Global store for rank
 
 var format = d3.time.format("%b %Y");
 var dataset;
 
-d3.csv("tennisdataset.csv", function(error, tennis) {
-if (error) {
-	return console.log(error);
-}
-tennis.forEach(function(d) {
-	d.price = +d.WRank;
-	//console.log(d);
-	// d.date = format.parse(d.date);
-});
+$(document).ready(function() {
+	d3.csv("newTennisData.csv", function(error, tennis) {
+		if (error) {
+			return console.log(error);
+		}
+		tennis.forEach(function(d) { //Convert data to Numbers
+			d.rank = +d.Rank;
+			d.wins = +d.Wins;
+			if (d.rank >= maxRank) {
+				maxRank = +d.rank;
+			}
+			if (d.wins >= maxWins) {
+				maxWins = +d.wins;
+			}
+		});
+		createGraphAxis();
+		createSliders();
 
-dataset = tennis;
-drawVis(dataset);
-});
+		dataset = tennis;
+		drawVis(dataset);
+	});
+});  
+
+var x, xAxis, y, yAxis;
+function createGraphAxis() {
+	x = d3.scale.linear()
+				.domain([0, maxRank])
+				.range([0, w]);
+
+	xAxis = d3.svg.axis()
+				.scale(x)
+				.orient("bottom"); 
+
+	y = d3.scale.linear()
+				.domain([0, maxWins])
+				.range([h, 0]);
+
+	yAxis = d3.svg.axis()
+				.scale(y)
+				.orient("left");
+
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + h + ")")
+		.call(xAxis)
+	.append("text")
+		.attr("x", w)
+		.attr("y", -6)
+		.style("text-anchor", "end")
+		.text("Avg. Rank");
+
+	svg.append("g")
+		.attr("class", "axis")
+		.call(yAxis)
+	.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("Wins");  
+}
+
 
 var mytype = "all"; //keep track of currently selected type; default is all 
 var patt = new RegExp("all");  
-function filterType(mtype)  {   
-	//console.log(mtype);  
+function filterType(category, mtype)  { 
 	mytype=mtype;       
 	var res = patt.test(mytype);       
 	if (res) {  
 		var toVisualize = dataset;  //use all the data        
 	} else {            
 		var toVisualize = dataset.filter(function(d, i) { //filter to only the selected type         
-			return d["Surface"] == mytype;  
+			return d[category] == mytype;  
 		});         
 	}
-	//console.log(toVisualize);     
 	drawVis(toVisualize); 
 } 
 
 
-function filterData(attr, values){
+function filterData(attributes, values){
 	// console.log(values);
 	// for (var i = 0; i < attributes.length; i++){
 	// 	if (attr == attributes[i]){
 	// 		ranges[i] = values;
 	// 	}
 	// }
-	var attributes = ["WPts"];
+	var attributes = ["rank"];
 	var ranges = [[values[0], values[1]]];
 	var toVisualize = dataset.filter(function(d) {
 	for (var i = 0; i < attributes.length; i++){ //for each attribute, return only if in range
@@ -107,61 +128,96 @@ function filterData(attr, values){
 	drawVis(toVisualize); 
 }
 
-$(function() {
-	$( "#slider-range" ).slider({
+function changeAxis(isYAxis, values) {
+	if (isYAxis) {//Do stuff to the Y
+		y = d3.scale.linear()
+					.domain([values[0], values[1]])
+					.range([h, 0]);
+		yAxis = d3.svg.axis()
+					.scale(y)
+					.orient("left");
+	} else { //Do stuff to the X
+		x = d3.scale.linear()
+			.domain([values[0], values[1]])
+			.range([0, w]);
+		xAxis = d3.svg.axis()
+					.scale(x)
+					.orient("bottom"); 
+	}
+	drawVis(dataset);
+}
+
+function createSliders() {
+	console.log(maxWins);
+	//Slider for the number of wins
+	$( "#slider-wins" ).slider({
 		range: true,
 		min: 0,
-		max: 14085,
-		values: [ 0, 14085],
+		max: maxWins,
+		values: [ 0, maxWins],
 		slide: function( event, ui ) {
-			$( "#amount" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-			filterData("WPts", ui.values);
+			$( "#amount-wins" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+			filterData(["wins"], ui.values);
+			changeAxis(true, ui.values);
 		}
 	});
-	$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-	  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-});
+	$( "#amount-wins" ).val($( "#slider-wins" ).slider( "values", 0 ) +
+	  " - " + $( "#slider-wins" ).slider( "values", 1 ));
+
+	//Slider for ranks
+	$( "#slider-rank" ).slider({
+		range: true,
+		min: 0,
+		max: maxRank,
+		values: [0, maxRank],
+		slide: function( event, ui ) {
+			$( "#amount-rank" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+			filterData(["wins"], ui.values);
+			changeAxis(false, ui.values);
+		}
+	});
+	$( "#amount-rank" ).val($( "#slider-rank" ).slider( "values", 0 ) +
+	  " - " + $( "#slider-rank" ).slider( "values", 1 ));
+}
 
 
 function drawVis(data) {
 //console.log("drawVis", data);
+	var div = d3.select("#graph").append("div")   
+		.attr("class", "tooltip")
+		.style("opacity", 0);
 
 
-var div = d3.select("#graph").append("div")   
-	.attr("class", "tooltip")
-	.style("opacity", 0);
-
-
-var circle = svg.selectAll("circle")            
- .data(data); //join with new data  
-		circle  //update existing circles – price, tValue, and type will change with type   
-		.attr("cx", function(d) { return x(d.WRank);  })            
-		.attr("cy", function(d) { return y(d.WPts);  })            
-		.style("fill", function(d) { return col(d.Surface); });                  
-		circle.exit().remove(); //remove any excess circles  
-		circle.enter().append("circle")  //add new circles  
-		.attr("cx", function(d) { return x(d.WRank);  })            
-		.attr("cy", function(d) { return y(d.WPts);  })            
-		.style("fill", function(d) { return col(d.Surface); })  
-		.attr("r", 4)  .style("stroke", "black")
-		.on("mouseover", function(d, i) {
-//			var $name   = $('div').html('Name: ' + d.name);
-//			var $type   = $('div').html('Type: ' + d.type);
-//			var $price  = $('div').html('Price: ' + d.price);
-//			var $tValue = $('div').html('tValue: ' + d.tValue);
-//			var $vol    = $('div').html('vol: ' + d.vol);
-			div.transition()        
-				.duration(200)      
-				.style("opacity", .9);      
-			div.html('<div>Name: ' + d.Winner + '</div><div>Type: ' + d.Surface + "</div><div>Rank: " + d.WRank + "</div><div>Points: "  + d.WPts + "</div><div>Location: " + d.Location)  
-			//div.html($name + $type + $price + $tValue + $vol)
-				.style("left", (d3.event.pageX) + "px")     
-				.style("top", (d3.event.pageY - 28) + "px");
-			})
-		.on("mouseout", function(d, i) { 
-			 div.transition()                
-					.duration(500)                
-					.style("opacity", 0);   
-		});	
-}
-});
+	var circle = svg.selectAll("circle")            
+	 .data(data); //join with new data  
+			circle  //update existing circles – price, tValue, and type will change with type   
+			.attr("cx", function(d) { return x(d.rank);  })            
+			.attr("cy", function(d) { return y(d.wins);  })            
+			.style("fill", function(d) { return col(d.Surface); });                  
+			circle.exit().remove(); //remove any excess circles  
+			
+			circle.enter().append("circle")  //add new circles  
+			.attr("cx", function(d) { return x(d.rank);  })            
+			.attr("cy", function(d) { return y(d.wins);  })            
+			.style("fill", function(d) { return col(d.Surface); })  
+			.attr("r", 4)  .style("stroke", "black")
+			.on("mouseover", function(d, i) {
+	//			var $name   = $('div').html('Name: ' + d.name);
+	//			var $type   = $('div').html('Type: ' + d.type);
+	//			var $price  = $('div').html('Price: ' + d.price);
+	//			var $tValue = $('div').html('tValue: ' + d.tValue);
+	//			var $vol    = $('div').html('vol: ' + d.vol);
+				div.transition()        
+					.duration(200)      
+					.style("opacity", .9);      
+				div.html('<div>Name: ' + d.Name + '</div><div>Surface: ' + d.Surface + "</div><div>Avg Rank: " + d.rank + "</div><div>Wins: "  + d.wins + "</div><div>Gender: " + d.Gender + "</div>")  
+				//div.html($name + $type + $price + $tValue + $vol)
+					.style("left", (d3.event.pageX) + "px")     
+					.style("top", (d3.event.pageY - 28) + "px");
+				})
+			.on("mouseout", function(d, i) { 
+				 div.transition()                
+						.duration(500)                
+						.style("opacity", 0);   
+			});	
+	}
