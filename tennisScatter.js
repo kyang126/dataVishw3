c3e153190233dba130f5f0a14e7e53f89c8ea4b5
Steven Raden <stevengraden@gmail.com>
2015-05-05 21:40:24 -0700

    Everything is working including the summed data

--- tennisScatter.js
+++ tennisScatter.js
@@ -35,7 +35,7 @@ var winsMapArr = [];
 var format     = d3.time.format("%b %Y");
 
 var attributes = ["Rank", "Wins"];
-var ranges     = [[0,maxRank],[0,maxWins]];
+var ranges; // format = [[minRank, maxRank], [minWins, MaxWins]]
 
 var dataset;
 var winsBegin  = 0;
@@ -59,15 +59,13 @@ $(document).ready(function() {
 			if (d.rank >= maxRank) {
 				maxRank = +d.rank;
 			}
-			// if (d.wins >= maxWins) {
-			// 	maxWins = +d.wins;
-			// }
 			countMap(d);
 		});
 		rankEnd = maxRank;
-		console.log("maxrank " + rankEnd + " " + maxRank);
 		winsMapArr = convertMapToArray(winsMap);
-		//console.log(winsMap);
+
+		ranges = [[0, maxRank], [0, maxWins]]; //Initialized after MaxRank/MaxWins started
+
 		createGraphAxis();
 		createSliders();
 
@@ -77,12 +75,12 @@ $(document).ready(function() {
 	$("#surfaceSelect").change(function(){ 
 		var value = $('#surfaceSelect').val();  
 		currentSurface = value;                      
-		filterType(value);                  
+		filterData();                  
 	});
 	$("#genderSelect").change(function(){                   
 		var value = $('#genderSelect').val(); 
 		currentGender = value;                       
-		filterType(value);                  
+		filterData();                  
 	});
 }); 
 
@@ -118,8 +116,7 @@ function convertMapToArray(map) {
 			maxWins = d.wins;
 		}
 	});
-	winsEnd= maxWins
-	//console.log(maxWins);
+	winsEnd = maxWins
 	return arr;
 }
 
@@ -161,68 +158,36 @@ function createGraphAxis() {
 		.style("text-anchor", "end")
 		.text("Wins");  
 }
-
-//filter info from the dropdown
-//Type = currently selected type in dropdown
-var patt = new RegExp("all");  
-function filterType(type)  { 
-	var res = patt.test(currentSurface);
-	var res2 = patt.test(currentGender);       
-	// if (res && res2) {
-	// 	var toVisualize = winsMapArr;  //use all the data       
-	// } else {
-		var toVisualize = dataset.filter(function(d, i) { //filter to only the selected type
+  
+//When a filter is changed call this to handle it
+function filterData()  { 
+	var toVisualize;
+	if (currentGender == "all" && currentSurface == "all") { //If both set to all then filter winsMapArr
+		toVisualize = winsMapArr.filter(function (d, i) {
+			if (d.rank >= ranges[0][0] && d.rank <= ranges[0][1] &&
+					d.wins >= ranges[1][0] && d.wins <= ranges[1][1]) { //Is it within the ranges
+				return true;
+			}
+			return false;
+		});
+	} else {
+		toVisualize = dataset.filter(function(d, i) { //filter to only the selected type
 			if (d[attributes[0]] >= ranges[0][0] && d[attributes[0]] <= ranges[0][1] &&
- 				d[attributes[1]] >= ranges[1][0] && d[attributes[1]] <= ranges[1][1]) {
-
-				if(currentGender == "all" && currentSurface == "all"){
-					return d[attributes[0]] >= ranges[0][0] && d[attributes[0]] <= ranges[0][1] &&
- 				d[attributes[1]] >= ranges[1][0] && d[attributes[1]] <= ranges[1][1];
-				}
-
+						d[attributes[1]] >= ranges[1][0] && d[attributes[1]] <= ranges[1][1]) { //Is it within the ranges
+				
 				if (currentGender != "all" && currentSurface != "all") { //If neither is set to all
 					return d["Gender"] == currentGender && d["Surface"] == currentSurface;
 				} else if (currentGender == "all") { //If gender is set to all
 					return d["Surface"] == currentSurface;
 				} else if (currentSurface == "all") { //It surface is set to all
 					return d["Gender"] == currentGender;
-				} else { //everything is set to all
-					return true;
 				}
 			}
 			return false;	
 		});
-	//}
+	}
 	drawVis(toVisualize); 
 } 
-
-//Filter info from the sliders
-//Attr = wins/ranks
-//Values = min/max of slider
-function filterData(attr, values){   
-	var toVisualize = dataset.filter(function(d, i) { 
-		if (d[attributes[0]] >= ranges[0][0] && d[attributes[0]] <= ranges[0][1] &&
- 				d[attributes[1]] >= ranges[1][0] && d[attributes[1]] <= ranges[1][1]) {
-
-				if(currentGender == "all" && currentSurface == "all"){
-					return d[attributes[0]] >= ranges[0][0] && d[attributes[0]] <= ranges[0][1] &&
- 				d[attributes[1]] >= ranges[1][0] && d[attributes[1]] <= ranges[1][1];
-				}
-
-				if (currentGender != "all" && currentSurface != "all") { //If neither is set to all
-					return d["Gender"] == currentGender && d["Surface"] == currentSurface;
-				} else if (currentGender == "all") { //If gender is set to all
-					return d["Surface"] == currentSurface;
-				} else if (currentSurface == "all") { //If surface is set to all
-					return d["Gender"] == currentGender;
-				} else { //everythign is set to all
-					return true;
-				}
-			}
-			return false;
-	});
-	drawVis(toVisualize); 
-}
 
 function changeAxis(isYAxis, values) {
 	if (isYAxis) {//Do stuff to the Y
@@ -257,7 +222,7 @@ function createSliders() {
 			winsBegin = ui.values[0];
 			winsEnd = ui.values[1];
 			ranges = [[rankBegin, rankEnd],[winsBegin, winsEnd]];
-			filterData(["Wins"], ui.values);
+			filterData();
 			changeAxis(true, ui.values);
 		}
 	});
@@ -274,7 +239,7 @@ function createSliders() {
 			rankBegin = ui.values[0];
 			rankEnd   = ui.values[1];
 			ranges    = [[rankBegin,rankEnd], [winsBegin,winsEnd]];
-			filterData(["Rank"], ui.values);
+			filterData();
 			changeAxis(false, ui.values);
 		}
 	});
@@ -288,32 +253,29 @@ function drawVis(data) {
 		.attr("class", "tooltip")
 		.style("opacity", 0);
 
-
-	var circle = svg.selectAll("circle")            
-	 .data(data); //join with new data  
-			circle  //update existing circles – price, tValue, and type will change with type   
-			.attr("cx", function(d) { return x(d.rank);  })            
-			.attr("cy", function(d) { return y(d.wins);  })            
-			.style("fill", function(d) { return col(d.Surface); });                  
-			circle.exit().remove(); //remove any excess circles  
-			
-			circle.enter().append("circle")  //add new circles  
-			.attr("cx", function(d) { return x(d.rank);  })            
-			.attr("cy", function(d) { return y(d.wins);  })            
-			.style("fill", function(d) { return col(d.Surface); })  
-			.attr("r", 4)  .style("stroke", "black")
-			.on("mouseover", function(d, i) {
-				div.transition()        
-					.duration(200)      
-					.style("opacity", .9);      
-				div.html('<div>Name: ' + d.Name + '</div><div>Surface: ' + d.Surface + "</div><div>Avg Rank: " + d.rank + "</div><div>Wins: "  + d.wins + "</div><div>Gender: " + d.Gender + "</div>")  
-				//div.html($name + $type + $price + $tValue + $vol)
-					.style("left", (d3.event.pageX) + "px")     
-					.style("top", (d3.event.pageY - 28) + "px");
-				})
-			.on("mouseout", function(d, i) { 
-				 div.transition()                
-						.duration(500)                
-						.style("opacity", 0);   
-			});	
-	}
+	var circle = svg.selectAll("circle").data(data); //join with new data  
+	circle  //update existing circles – price, tValue, and type will change with type   
+		.attr("cx", function(d) { return x(d.rank);  })            
+		.attr("cy", function(d) { return y(d.wins);  })            
+		.style("fill", function(d) { return col(d.Surface); });                  
+		circle.exit().remove(); //remove any excess circles  
+		
+		circle.enter().append("circle")  //add new circles  
+		.attr("cx", function(d) { return x(d.rank);  })            
+		.attr("cy", function(d) { return y(d.wins);  })            
+		.style("fill", function(d) { return col(d.Surface); })  
+		.attr("r", 4)  .style("stroke", "black")
+		.on("mouseover", function(d, i) {
+			div.transition()        
+				.duration(200)      
+				.style("opacity", .9);      
+			div.html('<div>Name: ' + d.Name + '</div><div>Surface: ' + d.Surface + "</div><div>Avg Rank: " + d.rank + "</div><div>Wins: "  + d.wins + "</div><div>Gender: " + d.Gender + "</div>")  
+				.style("left", (d3.event.pageX) + "px")     
+				.style("top", (d3.event.pageY - 28) + "px");
+			})
+		.on("mouseout", function(d, i) { 
+			 div.transition()                
+					.duration(500)                
+					.style("opacity", 0);   
+		});	
+}
